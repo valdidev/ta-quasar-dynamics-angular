e38956a019fbfc28d016528b0b26166c4b72dc70
“valdidev” <iamvaldidev@gmail.com>
2023-02-05 18:03:33 +0100

    feat: dashboard lazy routes

--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersPageComponent } from './components/users-page/users-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: UsersPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { UsersPageComponent } from './components/users-page/users-page.component';
+import { UserDetailsPageComponent } from './components/user-details-page/user-details-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UsersPageComponent,
+    UserDetailsPageComponent
+  ],
   imports: [
     CommonModule,
     DashboardRoutingModule
