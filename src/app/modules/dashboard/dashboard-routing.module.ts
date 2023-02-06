ed7cc69baabbbbe6abfc51ec129b3a806d37dfb1
“valdidev” <iamvaldidev@gmail.com>
2023-02-06 10:19:44 +0100

    wip: routes of dashboard

--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+// components
 import { DashboardContainerComponent } from './container/dashboard-container.component';
+import { UsersPageComponent } from './components/users-page/users-page.component';
+import { UserDetailsPageComponent } from './components/user-details-page/user-details-page.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: DashboardContainerComponent
-  }
+    component: DashboardContainerComponent,
+    children: [
+      {
+        path: '',
+        component: UsersPageComponent,
+      },
+      {
+        path: 'user/:userId',
+        component: UserDetailsPageComponent,
+      },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class DashboardRoutingModule { }
+export class DashboardRoutingModule {}
