4544f7f4ab3fc4706a2d821a71818e577bc72aba
“valdidev” <iamvaldidev@gmail.com>
2023-02-06 19:16:48 +0100

    style: auth styles

--- src/app/modules/auth/components/register-page/register-page.component.ts
+++ src/app/modules/auth/components/register-page/register-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-page',
@@ -6,5 +9,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./register-page.component.css']
 })
 export class RegisterPageComponent {
+  public form: FormGroup;
+  public isLoading: boolean = false;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(5)]],
+    });
+  }
+
+  // mocking login for now //TODO: fake login
+  tryRegister() {
+    const { email, password } = this.form.value;
+
+    if (email === 'test@test.com' && password === '123123') {
+      console.log('nice');
+      this.fakeDelay();
+    } else {
+      this.openSnackBar();
+      this.form.reset();
+    }
+  }
+
+  // mocking delay //TODO: fake delay
+  fakeDelay() {
+    this.isLoading = true;
+    setTimeout(() => {
+      // redirect to //TODO: redirect to
+      this.router.navigate(['/', 'auth', 'login']);
+    }, 1500);
+  }
+
+  // TODO: service
+  openSnackBar() {
+    this.snackBar.open('Wrong email or password', '', {
+      duration: 3000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+    });
+  }
 }
