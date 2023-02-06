bb8288abacbc31bf6dacd47e7306bb7b78f3841e
“valdidev” <iamvaldidev@gmail.com>
2023-02-06 11:18:57 +0100

    wip: users table hardcoded for now

--- src/app/modules/dashboard/components/users-page/users-page.component.ts
+++ src/app/modules/dashboard/components/users-page/users-page.component.ts
@@ -1,24 +1,14 @@
 import { Component } from '@angular/core';
+import { User } from '@shared/interfaces/user';
 
-export interface PeriodicElement {
-  //TODO: users interface
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+const usersList: User[] = [
+  {
+    id: 7,
+    email: 'michael.lawson@reqres.in',
+    first_name: 'Michael',
+    last_name: 'Lawson',
+    avatar: 'https://reqres.in/img/faces/7-image.jpg',
+  },
 ];
 
 @Component({
@@ -28,5 +18,5 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class UsersPageComponent {
   public displayedColumns: string[] = ['id', 'email', 'name', 'surname'];
-  public dataSource = ELEMENT_DATA;
+  public dataSource = usersList;
 }
