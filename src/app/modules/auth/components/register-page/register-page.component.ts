e10e655e076a3cf9f4cd5983426f3870cb9a8c28
“valdidev” <iamvaldidev@gmail.com>
2023-02-06 19:39:22 +0100

    refactor: reactive register form

--- src/app/modules/auth/components/register-page/register-page.component.ts
+++ src/app/modules/auth/components/register-page/register-page.component.ts
@@ -1,38 +1,52 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { passwordMatchValidator } from '@shared/utils/utils';
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
-  styleUrls: ['./register-page.component.css']
+  styleUrls: ['./register-page.component.css'],
 })
-export class RegisterPageComponent {
-  public form: FormGroup;
+export class RegisterPageComponent implements OnInit {
+  registerForm: FormGroup = new FormGroup({});
   public isLoading: boolean = false;
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private snackBar: MatSnackBar,
-    private router: Router
-  ) {
-    this.form = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(5)]],
-    });
+  constructor(private snackBar: MatSnackBar, private router: Router) {}
+
+  ngOnInit(): void {
+    this.registerForm = new FormGroup(
+      {
+        email: new FormControl('', [
+          Validators.required,
+          Validators.minLength(10),
+        ]),
+        password: new FormControl('', [
+          Validators.required,
+          Validators.minLength(6),
+        ]),
+        confirmPassword: new FormControl('', [
+          Validators.required,
+          Validators.minLength(6),
+        ]),
+      },
+      {
+        validators: passwordMatchValidator(this.registerForm),
+      }
+    );
   }
 
   // mocking login for now //TODO: fake login
   tryRegister() {
-    const { email, password } = this.form.value;
+    const { email, password } = this.registerForm.value;
 
     if (email === 'test@test.com' && password === '123123') {
       console.log('nice');
       this.fakeDelay();
     } else {
       this.openSnackBar();
-      this.form.reset();
+      this.registerForm.reset();
     }
   }
 
