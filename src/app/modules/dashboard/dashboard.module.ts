1141205318f257de55f728724c3cd4d0a5fdd913
“valdidev” <iamvaldidev@gmail.com>
2023-02-06 10:06:26 +0100

    style: navbar done

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -5,16 +5,20 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 // components
 import { UsersPageComponent } from './components/users-page/users-page.component';
 import { UserDetailsPageComponent } from './components/user-details-page/user-details-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MaterialModule } from '@shared/material.module';
 
 
 @NgModule({
   declarations: [
     UsersPageComponent,
-    UserDetailsPageComponent
+    UserDetailsPageComponent,
+    NavbarComponent
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    MaterialModule
   ]
 })
 export class DashboardModule { }
--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -7,6 +7,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
@@ -17,6 +19,8 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatButtonModule,
     MatSnackBarModule,
     MatProgressSpinnerModule,
+    MatToolbarModule,
+    MatIconModule,
   ],
   exports: [
     MatFormFieldModule,
@@ -24,6 +28,8 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatButtonModule,
     MatSnackBarModule,
     MatProgressSpinnerModule,
+    MatToolbarModule,
+    MatIconModule,
   ],
 })
 export class MaterialModule {}
