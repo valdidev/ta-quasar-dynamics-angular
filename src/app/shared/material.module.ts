64996350d7cdb74736906b36690af458b3cb4e39
“valdidev” <iamvaldidev@gmail.com>
2023-02-06 16:54:04 +0100

    wip: style: create users form

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -6,6 +6,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatButtonModule } from '@angular/material/button';
@@ -13,6 +14,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSortModule } from '@angular/material/sort';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [],
@@ -23,12 +25,14 @@ import { MatSortModule } from '@angular/material/sort';
     MatButtonModule,
     MatSnackBarModule,
     MatProgressSpinnerModule,
+    MatGridListModule,
     MatToolbarModule,
     MatIconModule,
     MatTableModule,
     MatTooltipModule,
     MatPaginatorModule,
     MatSortModule,
+    MatCardModule,
   ],
   exports: [
     MatFormFieldModule,
@@ -36,12 +40,14 @@ import { MatSortModule } from '@angular/material/sort';
     MatButtonModule,
     MatSnackBarModule,
     MatProgressSpinnerModule,
+    MatGridListModule,
     MatToolbarModule,
     MatIconModule,
     MatTableModule,
     MatTooltipModule,
     MatPaginatorModule,
     MatSortModule,
+    MatCardModule,
   ],
 })
 export class MaterialModule {}
