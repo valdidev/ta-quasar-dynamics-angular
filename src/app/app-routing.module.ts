aceae049b0e9ec09196f9c5455bce11d2894c9df
“valdidev” <iamvaldidev@gmail.com>
2023-02-07 18:26:42 +0100

    fix: route privacy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlreadyLoggedGuard } from '@core/guards/already-logged.guard';
 import { ValidateSessionGuard } from '@core/guards/validate-session.guard';
 
 const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('@modules/dashboard/dashboard.module').then(
+        (m) => m.DashboardModule
+      ),
+    canActivate: [ValidateSessionGuard],
+  },
   {
     // public
     path: 'auth',
     loadChildren: () =>
       import('@modules/auth/auth.module').then((m) => m.AuthModule),
+    canActivate: [AlreadyLoggedGuard],
   },
   {
     // private
--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
   },
   {
     path: '**',
-    redirectTo: '/auth/login',
+    redirectTo: 'login',
   },
 ];
 
